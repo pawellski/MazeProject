993e09783e459d9eb30b50f17fd1e814ec26a135
Paweł <skibaapawell@gmail.com>
2019-11-19 14:51:47 +0100

    Dodanie klasy Cell

---
+++ MazeProject/src/maze/Cell.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maze;
+
+/**
+ *
+ * @author Paweł
+ */
+public enum Cell {
+    ENTRANCE,
+    EXIT,
+    WALLL,
+    FIELD
+}
