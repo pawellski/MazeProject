c502db303ecbe0801a0c238e0ad354dee826d684
Paweł <skibaapawell@gmail.com>
2019-11-26 23:42:46 +0100

    Stworzenie algorytmu rozwiazujacego labirynt

--- MazeProject/src/algorithms/BFS.java
+++ MazeProject/src/algorithms/BFS.java
@@ -6,6 +6,7 @@
 package algorithms;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import maze.Cell;
 import maze.Maze;
@@ -21,19 +22,52 @@ public class BFS {
     private List<Node> nodes;
     private static int index;
     private Maze maze;
+    private LinkedList<Node> way;
 
     public BFS(Maze maze) {
-        nodes = new ArrayList<Node>();
+        this.nodes = new ArrayList<Node>();
         this.maze = maze;
+        this.way = new LinkedList<Node>();
+    }
+
+    public void createAndSolveMaze() {
+        createGraph();
+        solveMaze();
     }
 
     public void createGraph() {
         readNode();
         createEdges();
-        chooseEntrance();
-        chooseExit();
-        System.out.println("Entrance: " + entrance.getID());
-        System.out.println("Exit: " + exit.getID());
+        searchEntrance();
+        searchExit();
+    }
+
+    public void solveMaze() {
+        searchWay();
+        showWay();
+    }
+
+    public void searchWay() {
+        way.add(entrance);
+        while (way.getFirst() != exit) {
+            if (way.getFirst().getLeft() != null && way.getFirst().getLeft() != way.getFirst().getPredecessor()) {
+                way.add(way.getFirst().getLeft());
+                way.getLast().setPredecessor(way.getFirst());
+            }
+            if (way.getFirst().getTop() != null && way.getFirst().getTop() != way.getFirst().getPredecessor()) {
+                way.add(way.getFirst().getTop());
+                way.getLast().setPredecessor(way.getFirst());
+            }
+            if (way.getFirst().getRight() != null && way.getFirst().getRight() != way.getFirst().getPredecessor()) {
+                way.add(way.getFirst().getRight());
+                way.getLast().setPredecessor(way.getFirst());
+            }
+            if (way.getFirst().getBottom() != null && way.getFirst().getBottom() != way.getFirst().getPredecessor()) {
+                way.add(way.getFirst().getBottom());
+                way.getLast().setPredecessor(way.getFirst());
+            }
+            way.removeFirst();
+        }
     }
 
     private void readNode() {
@@ -53,26 +87,27 @@ public class BFS {
     private void createEdgesForNode(int x) {
         int i = 1 + 2 * (x / maze.getWidth());
         int j = 1 + 2 * (x % maze.getWidth());
-        
+
+        //left
         if (maze.getCell(i, j - 1) == Cell.FIELD) {
-            //left
             nodes.get(x).setLeft(nodes.get(x - 1));
         }
+        //top
         if (maze.getCell(i - 1, j) == Cell.FIELD) {
             //top
             nodes.get(x).setTop(nodes.get(x - maze.getWidth()));
         }
+        //right
         if (maze.getCell(i, j + 1) == Cell.FIELD) {
-            //right
             nodes.get(x).setRight(nodes.get(x + 1));
         }
+        //bottom
         if (maze.getCell(i + 1, j) == Cell.FIELD) {
-            //bottom
             nodes.get(x).setBottom(nodes.get(x + maze.getWidth()));
         }
     }
 
-    private void chooseEntrance() {
+    private void searchEntrance() {
         for (int x = 0; x < maze.getWidth(); x++) {
             int j = 1 + 2 * (x % maze.getWidth());
             if (maze.getCell(0, j) == Cell.ENTRANCE) {
@@ -81,7 +116,7 @@ public class BFS {
         }
     }
 
-    private void chooseExit() {
+    private void searchExit() {
         for (int x = (maze.getHeight() - 1) * maze.getWidth(); x < nodes.size(); x++) {
             int j = 1 + 2 * (x % maze.getWidth());
             if (maze.getCell(2 * maze.getHeight(), j) == Cell.EXIT) {
@@ -90,11 +125,12 @@ public class BFS {
         }
     }
 
-    public void printer() {
-        for (Node x : nodes) {
-            System.out.println(x.getID());
+    public void showWay() {
+        Node temporary = exit;
+        while (temporary != entrance) {
+            System.out.print(temporary.getID() + " -> ");
+            temporary = temporary.getPredecessor();
         }
-        System.out.println("Index to" + index);
+        System.out.println(entrance.getID());
     }
-
 }
