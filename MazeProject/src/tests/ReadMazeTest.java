2c02cfff792fb1f23bb06dbb5fb160dc8d43e97e
Paweł <skibaapawell@gmail.com>
2019-12-01 17:11:53 +0100

    Dodanie klasy testujacej funkcjonalnosc wczytywania labiryntu

---
+++ MazeProject/src/tests/ReadMazeTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import java.io.FileNotFoundException;
+import maze.Maze;
+
+/**
+ *
+ * @author Paweł
+ */
+public class ReadMazeTest {
+
+    private Maze maze;
+
+    public ReadMazeTest(String filename) {
+        try {
+            maze = new Maze(filename);
+        } catch (FileNotFoundException e) {
+            System.out.println("The file does not exist!");
+        }
+    }
+
+    public void drawMaze() {
+        if (maze != null) {
+            maze.drawMaze();
+        } else {
+            System.out.println("The maze did not create!");
+        }
+    }
+
+}
