7f6263bc5864737107e0349aa4258aea008c80eb
Paweł <skibaapawell@gmail.com>
2019-12-01 20:06:38 +0100

    Uruchomienie stworzonych testow funkcjonalnych w klasie Main

--- MazeProject/src/main/Main.java
+++ MazeProject/src/main/Main.java
@@ -8,6 +8,12 @@ package main;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import maze.Maze;
+import tests.BFSSolvedTest;
+import tests.CompareAlgorithms;
+import tests.GenerateMazeTest;
+import tests.ReadMazeTest;
+import tests.TremauxSolvedTest;
+import tests.WriteMazeTest;
 
 /**
  *
@@ -16,12 +22,44 @@ import maze.Maze;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        Maze maze1 = new Maze(10, 10);
-        maze1.generate();
-        maze1.drawMaze();
-        //maze1.writeMaze(fileName);
-        //Maze maze2 = new Maze(fileName);
-        //maze2.drawMaze();
+
+        System.out.println("GENERATE MAZE TEST");
+        GenerateMazeTest gmt = new GenerateMazeTest(10, 10);
+        System.out.println("The maze with one option solution.");
+        gmt.genrateAndDisplayMaze();
+        System.out.println();
+
+        System.out.println("The maze with a few solutions");
+        gmt.generateAndDisplayMazeWithMoreGaps();
+        System.out.println();
+
+        System.out.println("READ MAZE TEST");
+        ReadMazeTest rmt = new ReadMazeTest("src/resources/mazeToRead.txt");
+        System.out.println("The maze from file.");
+        rmt.drawMaze();
+        System.out.println();
+
+        System.out.println("WRITE MAZE TEST");
+        WriteMazeTest wmt = new WriteMazeTest("src/resources/savedMaze.txt", gmt.getMaze());
+        System.out.println();
+        
+        System.out.println("BFS SOLVED TEST");
+        BFSSolvedTest bfsst = new BFSSolvedTest();
+        bfsst.solveMaze();
+        System.out.println();
+
+        System.out.println("TREMAUX SOLVED TEST");
+        TremauxSolvedTest tst = new TremauxSolvedTest();
+        tst.solveMaze();
+        System.out.println();
+
+        System.out.println("COMPARE ALGORITHMS TEST");
+        System.out.println("The maze to solve:");
+        Maze maze = new Maze(30, 30);
+        maze.generate();
+        maze.drawMaze();
+        CompareAlgorithms ca = new CompareAlgorithms(maze);
+        ca.compareTime();
     }
 
 }
