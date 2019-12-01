350bfd9782af55a3ceb7833d842a5c847a61142e
Paweł <skibaapawell@gmail.com>
2019-12-01 17:11:03 +0100

    Dodanie klasy testujacej funkcjonalnosc rozwiazywania labiryntu przez algorytm Tremaux

---
+++ MazeProject/src/tests/TremauxSolvedTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import algorithms.Tremaux;
+import maze.Maze;
+
+/**
+ *
+ * @author Paweł
+ */
+public class TremauxSolvedTest {
+
+    private Tremaux tremaux;
+
+    public TremauxSolvedTest(Maze maze) {
+        tremaux = new Tremaux(maze);
+    }
+
+    public TremauxSolvedTest() {
+        Maze maze = new Maze(10, 10);
+        maze.generate();
+        tremaux = new Tremaux(maze);
+    }
+
+    public void solveMaze() {
+        tremaux.solveMaze();
+        tremaux.displaySolved();
+    }
+
+}
