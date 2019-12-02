b0da2459dcbdd758fcf537af75a88fab8008d033
Paweł <skibaapawell@gmail.com>
2019-12-02 22:21:55 +0100

    Zmiana nazwy

---
+++ MazeProject/src/tests/CompareAlgorithmsTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import algorithms.BFS;
+import algorithms.Tremaux;
+import maze.Maze;
+
+/**
+ *
+ * @author Paweł
+ */
+public class CompareAlgorithmsTest {
+
+    private BFS bfs;
+    private Tremaux treamux;
+
+    public CompareAlgorithmsTest(Maze maze) {
+        bfs = new BFS(maze);
+        treamux = new Tremaux(maze);
+    }
+
+    public void compareTime() {
+        long startBFS;
+        long stopBFS;
+        long startTremaux;
+        long stopTremaux;
+
+        startBFS = System.nanoTime();
+        bfs.createGraph();
+        bfs.searchWay();
+        stopBFS = System.nanoTime();
+        System.out.println();
+        System.out.println("Solutions BFS:");
+        bfs.showWay();
+        
+        startTremaux = System.nanoTime();
+        treamux.solveMaze();
+        stopTremaux = System.nanoTime();
+        System.out.println();
+        System.out.println("Solution Treamux:");
+        treamux.displaySolved();
+        
+        System.out.println("Czas dzialania algorytmu BFS = " + (stopBFS - startBFS));
+        System.out.println("Czas dzialania algorytmu Tremaux = " + (stopTremaux - startTremaux));
+
+        if( (stopBFS - startBFS) > (stopTremaux - startTremaux)){
+            System.out.println("Algorytm Treamux znalazl droge szybciej o " + ((stopBFS - startBFS) - (stopTremaux - startTremaux)));
+        } else {
+            System.out.println("Algorytm BFS znalazl droge szybciej o " + ((stopTremaux - startTremaux) - (stopBFS - startBFS)));
+        }
+    }
+}
